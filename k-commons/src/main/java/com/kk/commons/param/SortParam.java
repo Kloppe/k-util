ce7ad43d45ae64d599b6d852c3e731bd5ebb4809
juejin <juejin@gegejia.com>
2023-03-08 10:30:19 +0800

    ff

---
+++ k-commons/src/main/java/com/kk/commons/param/SortParam.java
@@ -0,0 +1,10 @@
+package com.kk.commons.param;
+/**
+ * SortParam
+ * 
+ *
+ * @author juejin
+ * @datetime 2023/3/7 
+ */
+public class SortParam {
+}
---
+++ k-rest/src/test/java/com/kk/rest/CopyTest.java
@@ -0,0 +1,10 @@
+package com.kk.rest;
+/**
+ * CopyTest
+ * 
+ *
+ * @author juejin
+ * @datetime 2023/3/8 
+ */
+public class CopyTest {
+}
